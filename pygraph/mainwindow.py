e23cb19ecbe10621d5a8a7c98d576b872fddbc9f
David Radice <david.radice@aei.mpg.de>
2011-08-19 10:54:15 +0200

    Added the plot widget to the main window

--- pygraph/mainwindow.py
+++ pygraph/mainwindow.py
@@ -1,4 +1,5 @@
-from PyQt4.Qt import QMainWindow
+from pygraph.plotwidget import PlotWidget
+from PyQt4.Qt import QMainWindow, QSettings, QSize, QPoint, QVariant
 import scidata.xgraph as xg
 import os.path
 
@@ -7,14 +8,35 @@ class MainWindow(QMainWindow):
     pygraph main window class
 
     Members
-    * datasets : a dictionary {filename: monodataset} storing the working data
-    * settings : a dictionary {option: value} storing the current setttings
+    * datasets   : a dictionary {filename: monodataset} storing the working data
+    * plotwidget : the plot widget
+    * settings   : a dictionary {option: value} storing the current setttings
     """
     datasets = {}
+    plotwidget = None
     settings = {}
 
     def __init__(self, args=None, options=None, parent=None):
         super(MainWindow, self).__init__(parent)
 
+        # Read data
         for fname in args:
             self.datasets[os.path.basename(fname)] = xg.parsefile(fname)
+
+        # Restore settings
+        qset = QSettings()
+
+        size = qset.value("MainWindow/Size", QVariant(QSize(600,400)))
+        self.resize(size.toSize())
+
+        position = qset.value("MainWindow/Position", QVariant(QPoint(0,0)))
+        self.move(position.toPoint())
+
+        # Create plot
+        self.plotwidget = PlotWidget(self)
+        self.setCentralWidget(self.plotwidget)
+
+    def closeEvent(self, event):
+        qset = QSettings()
+        qset.setValue("MainWindow/Size", QVariant(self.size()))
+        qset.setValue("MainWindow/Position", QVariant(self.pos()))
