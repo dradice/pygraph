8a54a6bc24ab4d56d7f38a961ab2bd299d58ffdb
Massimiliano Leoni <leoni.massimiliano1@gmail.com>
2011-08-29 17:25:00 +0200

    experimented axis scaling and labelling

--- pygraph/plotwidget.py
+++ pygraph/plotwidget.py
@@ -6,8 +6,12 @@ class PlotWidget(QwtPlot):
     def __init__(self, parent=None):
         super(PlotWidget, self).__init__(parent)
         self.setCanvasBackground(QColor("white"))
+        self.setAxisScale(2, 0, 1)
+        self.setAxisTitle(2, "X Axis")
+        self.setAxisScale(0, 0, 1)
+        self.setAxisTitle(0, "Y Axis")
 
         grid = QwtPlotGrid()
-#        grid.enableYMin(True)
+        grid.enableYMin(True)
         grid.setMinPen(QPen(DashLine))
         grid.attach(self)
