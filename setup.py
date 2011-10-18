c56a371c6babfd01f897e44b57171dfa985a0b5d
David Radice <david.radice@aei.mpg.de>
2011-10-18 17:41:48 +0200

    Fixed the setup script

--- setup.py
+++ setup.py
@@ -9,35 +9,17 @@ import os
 from os.path import join, splitext
 
 class QtBuild(build):
-    def compile_ui(self, ui_file, py_file=None):
-        # Search for pyuic4 in python bin dir, then in the $Path.
-        if py_file is None:
-            py_file = splitext(ui_file)[0] + "_ui.py"
-        try:
-            from PyQt4 import uic
-            fp = open(py_file, 'w')
-            uic.compileUi(ui_file, fp)
-            fp.close()
-            print "compiled", ui_file, "into", py_file
-        except Exception, e:
-            print 'Unable to compile user interface', e
-            return
-
     def compile_rc(self, qrc_file, py_file=None):
         # Search for pyuic4 in python bin dir, then in the $Path.
         if py_file is None:
-            py_file = splitext(qrc_file)[0] + "_rc.py"
+            py_file = splitext(qrc_file)[0] + ".py"
         if os.system('pyrcc4 "%s" -o "%s"' % (qrc_file, py_file)) > 0:
             print "Unable to generate python module for resource file", qrc_file
 
     def run(self):
-        # be sure to compile man page
-        os.system('make -C doc')
-        for dirpath, _, filenames in os.walk(join('hgviewlib', 'qt4')):
+        for dirpath, _, filenames in os.walk('pygraph'):
             for filename in filenames:
-                if filename.endswith('.ui'):
-                    self.compile_ui(join(dirpath, filename))
-                elif filename.endswith('.qrc'):
+                if filename.endswith('.qrc'):
                     self.compile_rc(join(dirpath, filename))
         build.run(self)
 
