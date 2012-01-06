52545c38db013aa143bc0ca5db1010b4bf75f586
Massimiliano Leoni <leoni.massimiliano1@gmail.com>
2012-01-06 17:23:51 +0100

    Cleaned and updated to version 0.1b

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ class QtBuild(build):
 
 setup(
 		name = 'pygraph',
-		version = '0.0',
+		version = '0.1b',
 		description = 'A freely available, lightweight and easy to use ' +
             'visualization client for viewing 1D data files.',
         author = 'David Radice',
