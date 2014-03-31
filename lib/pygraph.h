4ccc8c7fbd9e1ba78b5ac608d9a81cdeb7f5349f
David Radice <dradice@caltech.edu>
2014-03-30 20:14:55 -0700

    Added reference C implementation of the pyg file format

---
+++ lib/pygraph.h
@@ -0,0 +1,18 @@
+#ifndef PYGRAPH_H
+#define PYGRAPH_H
+
+#include <hdf5.h>
+
+/*
+ * pygraph native binary data format: reference implementation
+ */
+/* write 1D data in binary pygraph format and returns 0 on success */ 
+int pygwrite(
+        char const * fname,     /* [in] file name */
+        char mode,              /* [in] w:write a:append */
+        int const ts,           /* [in] current timestep */
+        float const ctime,      /* [in] current time */
+        int const np,           /* [in] number of tuples */
+        float const * data);    /* [in] x and y coordinates, interlaced */
+
+#endif
