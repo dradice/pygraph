59f411d72bbbedaae7235b6a0fb686fffff1cc92
David Radice <dradice@caltech.edu>
2014-03-30 18:28:40 -0700

    Added script to make the OS-X App

---
+++ make_app.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+MAKEICNS=/opt/local/bin/makeicns
+PYINSTALLER=/opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin/pyinstaller 
+
+mkdir -p export/OSX
+cd export/OSX
+
+$MAKEICNS \
+    -in ../../pygraph.jpg \
+    -out pygraph.icns
+
+$PYINSTALLER \
+    --hidden-import=h5py.h5ac \
+    --icon=pygraph.icns \
+    --windowed \
+    --onefile \
+    --strip \
+    ../../bin/pygraph
